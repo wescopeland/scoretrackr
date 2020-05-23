23a059ecb9aa140128f69d1d0e67b623f3935882
Wes Copeland <wlcopeland1@gmail.com>
2020-05-23 16:30:11 -0400

    feat: add material ui

---
+++ src/pages/_app.tsx
@@ -0,0 +1,37 @@
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { ThemeProvider } from "@material-ui/core/styles";
+import Head from "next/head";
+import React, { useEffect } from "react";
+
+import theme from "../theme";
+
+interface MyAppProps {
+  Component: any;
+  pageProps: any;
+}
+
+export default function MyApp({ Component, pageProps }: MyAppProps) {
+  useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector("#jss-server-side");
+    if (jssStyles?.parentElement) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+
+  return (
+    <>
+      <Head>
+        <title>My page</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
+}
