01ca0673123f97c8e63e413bbc409b6913095cd0
Wes Copeland <wlcopeland1@gmail.com>
2020-05-23 12:31:21 -0400

    ci: add cypress to the project

---
+++ cypress/plugins/cy-ts-preprocessor.js
@@ -0,0 +1,45 @@
+/* eslint-disable */
+
+const wp = require("@cypress/webpack-preprocessor");
+
+const webpackOptions = {
+  resolve: {
+    extensions: [".ts", ".js"]
+  },
+  node: { fs: "empty", child_process: "empty", readline: "empty" },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: [/node_modules/],
+        use: [
+          {
+            loader: "ts-loader"
+          }
+        ]
+      },
+      {
+        test: /\.feature$/,
+        use: [
+          {
+            loader: "cypress-cucumber-preprocessor/loader"
+          }
+        ]
+      },
+      {
+        test: /\.features$/,
+        use: [
+          {
+            loader: "cypress-cucumber-preprocessor/lib/featuresLoader"
+          }
+        ]
+      }
+    ]
+  }
+};
+
+const options = {
+  webpackOptions
+};
+
+module.exports = wp(options);
