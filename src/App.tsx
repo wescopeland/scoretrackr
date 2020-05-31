78fb0f0626b6c933de617b00b006599094bd9f17
Wes Copeland <wlcopeland1@gmail.com>
2020-05-31 19:35:55 -0400

    chore: delete dead files

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
-import './App.css';
 import { Home } from './Home';
 
 const App = () => (
