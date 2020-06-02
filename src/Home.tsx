d428e71a0ddfa520661f51f1c7f7f3d34ea6cb6e
Wes Copeland <hello@wescopeland.dev>
2020-06-02 18:03:42 -0400

    feat(recent-submissions): add time from now support

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,11 +1,14 @@
 import { Box, Typography } from '@material-ui/core';
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 
 import { AppBar } from './components/AppBar';
 import { Header } from './components/Header';
 import { MostRecentSubmissions } from './components/MostRecentSubmissions';
 
 export const Home = () => {
+  const { t } = useTranslation('common');
+
   return (
     <div>
       <Header />
@@ -14,7 +17,7 @@ export const Home = () => {
       <div className="container mt-5">
         <div className="row">
           <div className="col">
-            <Typography variant="h6">Most recent submissions</Typography>
+            <Typography variant="h6">{t('mostRecentSubmissions')}</Typography>
           </div>
         </div>
 
--- src/components/MostRecentSubmissions/MostRecentSubmissions.tsx
+++ src/components/MostRecentSubmissions/MostRecentSubmissions.tsx
@@ -1,54 +1,126 @@
 import { Box, Typography } from '@material-ui/core';
+import { formatDistanceToNow, isToday, isYesterday, parseISO } from 'date-fns';
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 
 import { RecentSubmission } from '../RecentSubmission';
 
+const donkeyKong = {
+  name: 'Donkey Kong',
+  friendlyId: 'dkong',
+  color: '#ba3448'
+};
+
+const donkeyKongJr = {
+  name: 'Donkey Kong Jr.',
+  friendlyId: 'dkongjr',
+  color: '#3451ba'
+};
+
+const recentSubmissions = [
+  {
+    date: '2020-06-02T16:27:06.461Z',
+    submissions: [
+      {
+        game: donkeyKong,
+        track: 'Factory settings',
+        playerAlias: 'Justin Elliott',
+        score: 1230000,
+        position: 3
+      },
+      {
+        game: donkeyKong,
+        track: 'Factory settings',
+        playerAlias: 'Wes Copeland',
+        score: 1218000,
+        position: 4
+      }
+    ]
+  },
+  {
+    date: '2020-06-01T16:27:06.461Z',
+    submissions: [
+      {
+        game: donkeyKongJr,
+        track: 'Factory settings',
+        playerAlias: 'Ben Falls',
+        score: 1420000,
+        position: 1
+      }
+    ]
+  },
+  {
+    date: '2020-05-14T16:27:06.461Z',
+    submissions: [
+      {
+        game: donkeyKong,
+        track: 'No hammer',
+        playerAlias: 'John Smith',
+        score: 700000,
+        position: 2
+      },
+      {
+        game: donkeyKongJr,
+        track: 'Factory settings',
+        playerAlias: 'John Doe',
+        score: 680000,
+        position: 38
+      },
+      {
+        game: donkeyKong,
+        track: 'Factory settings',
+        playerAlias: 'Jane Doe',
+        score: 417200,
+        position: 212
+      }
+    ]
+  }
+];
+
 export const MostRecentSubmissions = () => {
+  const { t } = useTranslation('common');
+
+  const getDateDistanceText = (date: string) => {
+    const parsedDate = parseISO(date);
+
+    if (isToday(parsedDate)) {
+      return t('today');
+    } else if (isYesterday(parsedDate)) {
+      return t('yesterday');
+    }
+
+    return formatDistanceToNow(parsedDate, { addSuffix: true });
+  };
+
   return (
     <>
-      <Typography variant="subtitle1" className="mb-1">
-        Today
-      </Typography>
+      {recentSubmissions.map((recentSubmission, index) => (
+        <div key={recentSubmission.date}>
+          <Typography
+            variant="subtitle1"
+            className={`mb-1 ${index !== 0 && 'mt-5'}`}
+          >
+            {getDateDistanceText(recentSubmission.date)}
+          </Typography>
 
-      <Box marginBottom={2}>
-        <RecentSubmission
-          gameColor="#ba3448"
-          gameFriendlyId="dkong"
-          gameName="Donkey Kong"
-          trackName="Factory settings"
-          playerAlias="Justin Elliott"
-          score={1230000}
-          position={3}
-        />
-      </Box>
-
-      <Box marginBottom={2}>
-        <RecentSubmission
-          gameColor="#ba3448"
-          gameFriendlyId="dkong"
-          gameName="Donkey Kong"
-          trackName="Factory settings"
-          playerAlias="Wes Copeland"
-          score={1218000}
-          position={4}
-        />
-      </Box>
-
-      <Typography variant="subtitle1" className="mt-4 mb-1">
-        Yesterday
-      </Typography>
-
-      <Box marginBottom={2}>
-        <RecentSubmission
-          gameColor="#3451ba"
-          gameFriendlyId="dkongjr"
-          gameName="Donkey Kong Jr."
-          trackName="Factory settings"
-          playerAlias="Ben Falls"
-          score={1420000}
-          position={1}
-        />
-      </Box>
+          {recentSubmission.submissions.map((submission) => (
+            <Box
+              key={`${submission.game} ${submission.playerAlias} ${submission.score}`}
+              marginBottom={2}
+            >
+              <RecentSubmission
+                gameColor={submission.game.color}
+                gameFriendlyId={submission.game.friendlyId}
+                gameName={submission.game.name}
+                trackName={submission.track}
+                playerAlias={submission.playerAlias}
+                score={submission.score}
+                position={submission.position}
+              />
+            </Box>
+          ))}
+        </div>
+      ))}
     </>
   );
 };
