b20a50e00e14bcf160988fbaa6b3d99ec2dfa55d
Wes Copeland <wlcopeland1@gmail.com>
2020-06-01 22:56:14 -0400

    test(recent-submission): clean up duplication

--- src/components/RecentSubmission/RecentSubmission.test.tsx
+++ src/components/RecentSubmission/RecentSubmission.test.tsx
@@ -1,105 +1,49 @@
 import '@testing-library/jest-dom';
-import { cleanup, render } from '@testing-library/react';
+import { cleanup, render, RenderResult } from '@testing-library/react';
 import React from 'react';
 
 import { RecentSubmission } from './RecentSubmission';
 
 describe('Component: RecentSubmission', () => {
+  let rendered: RenderResult;
+
+  beforeEach(() => {
+    rendered = render(
+      <RecentSubmission
+        gameColor="#ba3448"
+        gameFriendlyId="dkong"
+        gameName="Donkey Kong"
+        trackName="Factory settings"
+        playerAlias="Justin Elliott"
+        score={1230000}
+        position={3}
+      />
+    );
+  });
+
   afterEach(cleanup);
 
   it('renders without crashing', () => {
-    const { container } = render(
-      <RecentSubmission
-        gameColor="#ba3448"
-        gameFriendlyId="dkong"
-        gameName="Donkey Kong"
-        trackName="Factory settings"
-        playerAlias="Justin Elliott"
-        score={1230000}
-        position={3}
-      />
-    );
-
-    expect(container).toBeVisible();
+    expect(rendered.container).toBeVisible();
   });
 
   it('shows the game name text', () => {
-    const { getByText } = render(
-      <RecentSubmission
-        gameColor="#ba3448"
-        gameFriendlyId="dkong"
-        gameName="Donkey Kong"
-        trackName="Factory settings"
-        playerAlias="Justin Elliott"
-        score={1230000}
-        position={3}
-      />
-    );
-
-    expect(getByText('Donkey Kong')).toBeVisible();
+    expect(rendered.getByText('Donkey Kong')).toBeVisible();
   });
 
   it('shows the track name text', () => {
-    const { getByText } = render(
-      <RecentSubmission
-        gameColor="#ba3448"
-        gameFriendlyId="dkong"
-        gameName="Donkey Kong"
-        trackName="Factory settings"
-        playerAlias="Justin Elliott"
-        score={1230000}
-        position={3}
-      />
-    );
-
-    expect(getByText('Factory settings')).toBeVisible();
+    expect(rendered.getByText('Factory settings')).toBeVisible();
   });
 
   it('shows the score text as a formatted number', () => {
-    const { getByText } = render(
-      <RecentSubmission
-        gameColor="#ba3448"
-        gameFriendlyId="dkong"
-        gameName="Donkey Kong"
-        trackName="Factory settings"
-        playerAlias="Justin Elliott"
-        score={1230000}
-        position={3}
-      />
-    );
-
-    expect(getByText('1,230,000')).toBeVisible();
+    expect(rendered.getByText('1,230,000')).toBeVisible();
   });
 
   it('shows the player name', () => {
-    const { getByText } = render(
-      <RecentSubmission
-        gameColor="#ba3448"
-        gameFriendlyId="dkong"
-        gameName="Donkey Kong"
-        trackName="Factory settings"
-        playerAlias="Justin Elliott"
-        score={1230000}
-        position={3}
-      />
-    );
-
-    expect(getByText('Justin Elliott')).toBeVisible();
+    expect(rendered.getByText('Justin Elliott')).toBeVisible();
   });
 
   it('shows the position as an ordinal number', () => {
-    const { getByText } = render(
-      <RecentSubmission
-        gameColor="#ba3448"
-        gameFriendlyId="dkong"
-        gameName="Donkey Kong"
-        trackName="Factory settings"
-        playerAlias="Justin Elliott"
-        score={1230000}
-        position={3}
-      />
-    );
-
-    expect(getByText('3rd')).toBeVisible();
+    expect(rendered.getByText('3rd')).toBeVisible();
   });
 });
