d32e889e0c386ea5e2d8c48a9f190789a748688a
Wes Copeland <wlcopeland1@gmail.com>
2020-06-14 11:41:14 -0400

    chore: clean up coverage stats

--- src/common/utils/get-base-url.ts
+++
@@ -1,9 +0,0 @@
-export const getBaseUrl = () => {
-  // This should only be present on a real production deployment.
-  // It's probably set to "https://scoretrac.kr".
-  if (process.env.HEROKU_PRODUCTION_BASE_URL) {
-    return process.env.HEROKU_PRODUCTION_BASE_URL;
-  }
-
-  return 'http://localhost:3000';
-};
