ee24b1d9e5239b829d47d83e14176211891f9062
Wes Copeland <wlcopeland1@gmail.com>
2020-06-19 23:21:41 -0400

    chore: remove unneeded comment

--- src/server/resolvers/track/submission-count.field-resolver.ts
+++ src/server/resolvers/track/submission-count.field-resolver.ts
@@ -5,7 +5,6 @@ import { db } from 'server/firebase-admin-app';
 
 export const submissionCountFieldResolver = async (track: Track) => {
   try {
-    // Find the game that contains this track.
     const trackGamesSnapshot = await db
       .firestore()
       .collection('scores')
