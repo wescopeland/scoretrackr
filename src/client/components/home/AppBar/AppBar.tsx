84859d021c39661039edb8ab264202bd0b33aef3
Wes Copeland <wlcopeland1@gmail.com>
2020-07-05 18:50:52 -0400

    feat(app-bar): add sign up and log in buttons

--- src/client/components/home/AppBar/AppBar.tsx
+++ src/client/components/home/AppBar/AppBar.tsx
@@ -1,10 +1,19 @@
-import { AppBar as MuiAppBar, Toolbar } from '@material-ui/core';
+import { AppBar as MuiAppBar, Button, Toolbar } from '@material-ui/core';
 import React from 'react';
+import { useTranslation } from 'react-i18next';
+
+import { useStyles } from './AppBar.styles';
 
 export const AppBar = () => {
+  const { t } = useTranslation('common');
+  const classes = useStyles();
+
   return (
     <MuiAppBar position="static">
-      <Toolbar variant="dense" />
+      <Toolbar variant="dense" className={classes.toolbar}>
+        <Button color="inherit">{t('signUp')}</Button>
+        <Button color="inherit">{t('logIn')}</Button>
+      </Toolbar>
     </MuiAppBar>
   );
 };
