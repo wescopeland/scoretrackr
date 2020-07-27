47a4a24c16d0c19291ac39c05e17f5d7eacbd802
Wes Copeland <wescopeland@Wess-MacBook-Pro.local>
2020-07-27 11:45:06 -0400

    feat(auth): use next-auth

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,48 @@
+import { PrismaClient } from '@prisma/client';
+import NextAuth from 'next-auth';
+import Providers from 'next-auth/providers';
+import { verify, argon2id } from 'argon2';
+
+const prisma = new PrismaClient();
+
+const options = {
+  providers: [
+    Providers.Credentials({
+      name: 'Credentials',
+      credentials: {
+        email: { label: 'Email', type: 'text' },
+        password: { label: 'Password', type: 'password' }
+      },
+      authorize: async ({ email, password }) => {
+        const user = await prisma.user.findOne({ where: { email } });
+
+        if (!user) {
+          return Promise.reject(new Error('Invalid credentials.'));
+        }
+
+        const isValid = await verify(user.password, password, {
+          type: argon2id
+        });
+
+        if (!isValid) {
+          return Promise.reject(new Error('Invalid credentials.'));
+        } else {
+          return Promise.resolve(user);
+        }
+      }
+    })
+  ],
+
+  callbacks: {
+    signIn: async (user, account, profile) => {
+      console.log({ user });
+      return Promise.resolve(false);
+    }
+  },
+
+  session: { jwt: true },
+
+  database: process.env.DATABASE_URL
+};
+
+export default (req, res) => NextAuth(req, res, options);
