10558f67da85d9a5fa22d5256645c153afe85e2c
Wes Copeland <wlcopeland1@gmail.com>
2020-08-01 17:26:06 -0400

    fix(api): dont confuse the ping routes

--- src/pages/api/ping.ts
+++ src/pages/api/ping.ts
@@ -1,12 +1,5 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
 export default async function (req: NextApiRequest, res: NextApiResponse) {
-  const channel = req.headers['Nightbot-Channel'];
-  const user = req.headers['Nightbot-User'];
-
-  res
-    .status(200)
-    .send(
-      `Scoretrackr Sheets API is returning a 200. Your username is ${user} and the current channel is ${channel}.`
-    );
+  res.status(200).send('Pong!');
 }
--- src/pages/api/sheets/ping.ts
+++ src/pages/api/sheets/ping.ts
@@ -1,7 +1,12 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
 export default async function (req: NextApiRequest, res: NextApiResponse) {
-  console.log(req);
-  res.setHeader('Content-Type', 'text/plain');
-  res.status(200).send('Pong!');
+  const channel = req.headers['Nightbot-Channel'];
+  const user = req.headers['Nightbot-User'];
+
+  res
+    .status(200)
+    .send(
+      `Scoretrackr Sheets API is returning a 200. Your username is ${user} and the current channel is ${channel}.`
+    );
 }
