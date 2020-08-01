24c20854b76181fe21602f25ede8a3f4ff5ad696
Wes Copeland <wlcopeland1@gmail.com>
2020-08-01 17:11:44 -0400

    fix: set content type

--- src/pages/api/sheets/ping.ts
+++ src/pages/api/sheets/ping.ts
@@ -2,5 +2,6 @@ import { NextApiRequest, NextApiResponse } from 'next';
 
 export default async function (req: NextApiRequest, res: NextApiResponse) {
   console.log(req);
+  res.setHeader('Content-Type', 'text/plain');
   res.status(200).send('Pong!');
 }
