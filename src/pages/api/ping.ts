61779430f357b2554a31a061d39d067b911b302c
Wes Copeland <wlcopeland1@gmail.com>
2020-08-01 17:22:44 -0400

    feat(sheets): update ping to show user and channel

--- src/pages/api/ping.ts
+++ src/pages/api/ping.ts
@@ -1,5 +1,12 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
 export default async function (req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).send('Ping!');
+  const channel = req.headers['Nightbot-Channel'];
+  const user = req.headers['Nightbot-User'];
+
+  res
+    .status(200)
+    .send(
+      `Scoretrackr Sheets API is returning a 200. Your username is ${user} and the current channel is ${channel}.`
+    );
 }
